ba356f1ca8e8f4e7ee83da3a8bc6f06e065ab5ec
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-09-01 12:36:58 +0900

    simpe setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='pyjsbn-rsa',
+      version='0.1',
+      description='Python RSA module which is compatible with jsbn.js',
+      author='Wonsup Yoon',
+      author_email='pusnow@yonsei.ac.kr',
+      url='https://github.com/Pusnow/pyjsbn-rsa',
+      packages =  ["jsbn"]
+     )
