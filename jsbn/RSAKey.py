aaa030e86351727411c2ad8d7a0c18f182cada8f
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-09-01 11:51:33 +0900

    rsa2.js implement ( Decrypt, Generate)

--- jsbn/RSAKey.py
+++ jsbn/RSAKey.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-from rsa import key, encrypt, PublicKey
+from rsa import key, encrypt, decrypt, PublicKey, PrivateKey, newkeys
 
 
 class RSAKey :
@@ -21,7 +21,6 @@ class RSAKey :
 		if N != None and E != None and len(N) > 0 and len(E) > 0:
 			self.n = int(N,16)
 			self.e = int(E,16)
-			self.pubkey = PublicKey(self.n, self.e)
 		else :
 			pass
 
@@ -30,6 +29,64 @@ class RSAKey :
 	def Encrypt(self, text):
 		if text == None:
 			return None
-		ciphertext = encrypt(text,self.pubkey)
+
+		pubkey = PublicKey(self.n, self.e)
+		ciphertext = encrypt(text,pubkey)
 		return ''.join([("%x"% ord(x)).zfill(2) for x in ciphertext])
+
+
+	#Set the private key fields N, e, and d from hex strings
+	def SetPrivate(self,N,E,D) :
+		if N != None and E != None and len(N) > 0 and len(E) > 0 :
+			self.n = int(N,16)
+			self.e = int(E,16)
+			self.d = int(D,16)
+		else :
+			pass
+
+
+	#Set the private key fields N, e, d and CRT params from hex strings
+	def SetPrivateEx(self,N,E,D,P,Q,DP,DQ,C):
+		if N != None and E != None and len(N) > 0 and len(E) > 0 :
+			self.n = int(N,16)
+			self.e = int(E,16)
+			self.d = int(D,16)
+			self.p = int(P,16)
+			self.q = int(Q,16)
+			self.dmp1 = int(DP,16)
+			self.dmq1 = int(DQ,16)
+			self.coeff = int(C,16)
+
+		else: 
+			pass
+
+	# Return the PKCS#1 RSA decryption of "ctext".
+	# "ctext" is an even-length hex string and the output is a plain string.
+	def Decrypt(self, ctext) :
+		import re
+		ctext = ''.join([chr(int(x, 16)) for x in re.findall(r'\w\w', ctext)])
+		#c = int(ctext,16)
+		prikey = PrivateKey(self.n, self.e, self.d, self.p, self.q, self.dmp1, self.dmq1, self.coeff)
+		return decrypt(ctext,prikey)
+
+
+	#Generate a new random private key B bits long, using public expt E
+	def Generate (self,B,E):
+		self.e = int(E,16)
+		(pubkey, prikey) = newkeys(B)
+		self.n = pubkey.n
+		self.e = pubkey.e
+		self.d = prikey.d
+		self.p = prikey.p
+		self.q = prikey.q
+		self.dmp1 = prikey.exp1
+		self.dmq1 = prikey.exp2
+		self.coeff = prikey.coef
+
+		
+
+
+
+
+
 
