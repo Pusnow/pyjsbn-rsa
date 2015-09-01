9e55cdf24f304c8a6424424d7295fe131fb96c7f
Wonsup Yoon <pusnow@yonsei.ac.kr>
2015-09-01 23:17:15 +0900

    change methods to camelcase

--- jsbn/RSAKey.py
+++ jsbn/RSAKey.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-from rsa import encrypt, decrypt, PublicKey, PrivateKey, newkeys
+import rsa
 
 
 class RSAKey :
@@ -17,7 +17,7 @@ class RSAKey :
 
 
 	#Set the public key fields N and e from hex strings
-	def SetPublic(self,N,E):
+	def setPublic(self,N,E):
 		if N != None and E != None and len(N) > 0 and len(E) > 0:
 			self.n = int(N,16)
 			self.e = int(E,16)
@@ -26,17 +26,17 @@ class RSAKey :
 
 
 	#Return the PKCS#1 RSA encryption of "text" as an even-length hex string
-	def Encrypt(self, text):
+	def encrypt(self, text):
 		if text == None:
 			return None
 
-		pubkey = PublicKey(self.n, self.e)
-		ciphertext = encrypt(text,pubkey)
+		pubkey = rsa.PublicKey(self.n, self.e)
+		ciphertext = rsa.encrypt(text,pubkey)
 		return ''.join([("%x"% ord(x)).zfill(2) for x in ciphertext])
 
 
 	#Set the private key fields N, e, and d from hex strings
-	def SetPrivate(self,N,E,D) :
+	def setPrivate(self,N,E,D) :
 		if N != None and E != None and len(N) > 0 and len(E) > 0 :
 			self.n = int(N,16)
 			self.e = int(E,16)
@@ -46,7 +46,7 @@ class RSAKey :
 
 
 	#Set the private key fields N, e, d and CRT params from hex strings
-	def SetPrivateEx(self,N,E,D,P,Q,DP,DQ,C):
+	def setPrivateEx(self,N,E,D,P,Q,DP,DQ,C):
 		if N != None and E != None and len(N) > 0 and len(E) > 0 :
 			self.n = int(N,16)
 			self.e = int(E,16)
@@ -62,18 +62,18 @@ class RSAKey :
 
 	# Return the PKCS#1 RSA decryption of "ctext".
 	# "ctext" is an even-length hex string and the output is a plain string.
-	def Decrypt(self, ctext) :
+	def decrypt(self, ctext) :
 		import re
 		ctext = ''.join([chr(int(x, 16)) for x in re.findall(r'\w\w', ctext)])
 		#c = int(ctext,16)
-		prikey = PrivateKey(self.n, self.e, self.d, self.p, self.q, self.dmp1, self.dmq1, self.coeff)
-		return decrypt(ctext,prikey)
+		prikey = rsa.PrivateKey(self.n, self.e, self.d, self.p, self.q, self.dmp1, self.dmq1, self.coeff)
+		return rsa.decrypt(ctext,prikey)
 
 
 	#Generate a new random private key B bits long, using public expt E
-	def Generate (self,B,E):
+	def generate (self,B,E):
 		self.e = int(E,16)
-		(pubkey, prikey) = newkeys(B)
+		(pubkey, prikey) = rsa.newkeys(B)
 		self.n = pubkey.n
 		self.e = pubkey.e
 		self.d = prikey.d
