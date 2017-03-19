eaedb06878ef12e483a079842869c446e75a94e7
Wonsup Yoon <pusnow@yonsei.ac.kr>
2017-03-20 03:20:30 +0900

    reformat

--- jsbn/__init__.py
+++ jsbn/__init__.py
@@ -1,2 +1,1 @@
-
 from .RSAKey import RSAKey
--- setup.py
+++ setup.py
@@ -5,31 +5,29 @@ from setuptools import setup
 from codecs import open
 from os import path
 
-
-setup(name='pyjsbn-rsa',
-      version='0.2.2',
-      description='Python RSA module compatible with jsbn.js',
-      long_description = "",
-      author='Wonsup Yoon',
-      author_email='pusnow@yonsei.ac.kr',
-      license='BSD',
-      url='https://github.com/Pusnow/pyjsbn-rsa',
-      classifiers=[
+setup(
+    name='pyjsbn-rsa',
+    version='0.2.2',
+    description='Python RSA module compatible with jsbn.js',
+    long_description="",
+    author='Wonsup Yoon',
+    author_email='pusnow@yonsei.ac.kr',
+    license='BSD',
+    url='https://github.com/Pusnow/pyjsbn-rsa',
+    classifiers=[
         'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Security :: Cryptography',
 
-        'License :: OSI Approved :: BSD License',
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-    	],
-    	keywords='jsbn rsa RSAKey',
-    	install_requires=['rsa'],
-		packages =  ["jsbn"],
-     )
+    ],
+    ],
+    keywords='jsbn rsa RSAKey',
+    install_requires=['rsa'],
+    packages=["jsbn"], )
